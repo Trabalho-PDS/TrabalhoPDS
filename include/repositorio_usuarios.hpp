1be8a280578e9cfa9e13e337db4a9ccb10b6e090
sarahComH <106473736+sarahComH@users.noreply.github.com>
2023-11-24 19:34:27 -0300

    Update repositorio_usuarios.hpp

--- include/repositorio_usuarios.hpp
+++ include/repositorio_usuarios.hpp
@@ -7,7 +7,7 @@
 
 class RepositorioUsuarios {
 public:
-  void criar_usuario(const std::string email, const std::string senha, const std::string tipo, std::string id_usuario = "default");
+  void criar_usuario(const std::string email, const std::string senha,  std::string const nome_completo, const std::string tipo, std::string id_usuario = "default");
 
   void remover_usuario(std::string id_usuario);
 
