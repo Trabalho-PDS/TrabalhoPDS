bbe0de1b25da3e0dc641ae6f1ed54b14e9fe3498
sarahComH <106473736+sarahComH@users.noreply.github.com>
2023-11-24 21:38:45 -0300

    Update repositorio_usuarios.hpp

--- include/repositorio_usuarios.hpp
+++ include/repositorio_usuarios.hpp
@@ -15,12 +15,11 @@ public:
 
   void editar_usuarios(std::string id_usuario, const std::string novo_email, const std::string nova_senha, const std::string tipo);
 
-  std::string identidade_usuario(std::string email,std::string senha);
+  std::string identidade_usuario(std::string email);
 
   std::string dados_usuario(const std::string idUsuario);
 private: 
   std::string _currentPath = std::filesystem::current_path().string();
-  std::string _authkeysPath = _currentPath.find("tests") ? _currentPath + "../../storage/authkeys.txt" : _currentPath + "../storage/authkeys.txt";
-  std::string _filePath = _currentPath.find("tests") ? _currentPath + "../../storage/usuario.txt" : _currentPath + "../storage/usuario.txt";
-
+std::string _authkeysPath = _currentPath + "/storage/authkeys.txt";
+  std::string _filePath = _currentPath + "/storage/usuario.txt";
 };
