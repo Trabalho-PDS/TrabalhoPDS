b4189c3ae81f93bf1a0b4282a244afbaa1f07e0f
sarahComH <106473736+sarahComH@users.noreply.github.com>
2023-11-30 21:30:45 -0300

    comentei repositorio_usuarios.cpp

--- src/repositorio_usuarios.cpp
+++ src/repositorio_usuarios.cpp
@@ -26,22 +26,20 @@ std::vector<std::string> splitString(const std::string &input, char delimiter) {
 }
 
 std::string generate_user_id(std::string const &email,
-                             std::string const &password) {
-  // Concatenate email and password
-  std::string combinedString = email + password;
+                             std::string const &password) { /*gera um id de usuário com base no email e na senha fornecidos*/
+  std::string combinedString = email + password; // uma string combinada que junta o email e a senha todas em uma
 
-  // Use std::hash to generate a hash value
-  std::hash<std::string> hasher;
+  std::hash<std::string> hasher; // vai usar o std::hash para gerar um valor hash para a string combinada e é armazenado em hashValue
   std::size_t hashValue = hasher(combinedString);
-
-  // Convert the hash value to an alphanumeric string
+  
   std::ostringstream ss;
-  ss << std::hex << hashValue;
+  ss << std::hex << hashValue; // essa é a conversão do valor hash do objeto para a string, que é o que nós queremos usar como id
 
   return ss.str();
 }
 
 bool binary_search(std::vector<std::string> vecOfStrings, std::string element) {
+  /*deixa a busca mais rápida, já que o authkeys.txt já é ordenado*/
   bool return_value =
       std::binary_search(vecOfStrings.begin(), vecOfStrings.end(), element);
   return return_value;
@@ -56,10 +54,11 @@ void RepositorioUsuarios::criar_usuario(const std::string email,
   // verifica se o usuário já existe:
 
   if (id_usuario == "default") {
-    id_usuario = generate_user_id(email, senha);
+    id_usuario = generate_user_id(email, senha);// a ideia é que, como o id do usuário é unico e o mesmo email e 
+    //mesma senha vão gerar o mesmo id, podemos ver se ele já está no arquivo
   }
 
-  std::ifstream AuthkeysFile;
+  std::ifstream AuthkeysFile; // arquivo aberto para a leitura
 
   std::vector<std::string> authkeysVec;
   authkeysVec.clear();
@@ -88,7 +87,7 @@ void RepositorioUsuarios::criar_usuario(const std::string email,
 
   authkeysVec.push_back(id_usuario);
 
-  // Use the custom comparator to ensure stable sort
+  // vai ordenar o vetor antes de adicionarmos sobrescrevermos o arquivo
   std::sort(authkeysVec.begin(), authkeysVec.end());
 
   std::ofstream AuthkeysFile_out;
@@ -104,13 +103,15 @@ void RepositorioUsuarios::criar_usuario(const std::string email,
     std::cerr << e.what();
     return;
   }
-
+/*
+  precisamos sobrescrever o arquivo pois ele sempre precisa estar ordenado
+*/
   authkeysVec.clear();
 
-  std::ofstream FileUsers;
+  std::ofstream FileUsers; 
 
   try {
-    FileUsers.open(RepositorioUsuarios::_filePath, std::ios::app);
+    FileUsers.open(RepositorioUsuarios::_filePath, std::ios::app); // inclui novas informações no arquivo:
     FileUsers << id_usuario + " " + email + " " + nome_completo + " " + tipo +
                      "\n";
 
@@ -145,7 +146,7 @@ void RepositorioUsuarios::remover_usuario(std::string const id_usuario) {
     return;
   }
 
-  std::ofstream AuthkeysFile_out;
+  std::ofstream AuthkeysFile_out; // o processo é basicamente o mesmo dos outros, mas dessa vez, não  vamos salvar no arquivo os usuários que possuem o mesmo id
 
   try {
     AuthkeysFile_out.open(RepositorioUsuarios::_authkeysPath);
@@ -203,7 +204,7 @@ void RepositorioUsuarios::remover_usuario(std::string const id_usuario) {
 bool RepositorioUsuarios::autenticar_login(std::string email,
                                            std::string senha) {
 
-  std::string id_usuario = generate_user_id(email, senha);
+  std::string id_usuario = generate_user_id(email, senha); // vai conferir, através do id se o usuário existe ou não. Ou seja: se os dados dele estão ou não corretos;
 
   std::ifstream AuthkeysFile;
 
@@ -230,7 +231,7 @@ bool RepositorioUsuarios::autenticar_login(std::string email,
 }
 
 std::string RepositorioUsuarios::identidade_usuario(std::string const email) {
-  std::ifstream FileUsers;
+  std::ifstream FileUsers; // arquivo para leitura
 
   try {
     FileUsers.open(RepositorioUsuarios::_filePath);
@@ -240,7 +241,7 @@ std::string RepositorioUsuarios::identidade_usuario(std::string const email) {
       std::stringstream str(line);
 
       std::vector<std::string> auxiliarLogin = splitString(line, ' ');
-      if (auxiliarLogin[1] == email) {
+      if (auxiliarLogin[1] == email) { // nesse caso, auxiliarLogin[1] representa o email do usuário, já que os dados dele são separados por um espaço
 
         FileUsers.close();
         return auxiliarLogin[0];
@@ -255,19 +256,20 @@ std::string RepositorioUsuarios::identidade_usuario(std::string const email) {
 }
 
 std::string RepositorioUsuarios::dados_usuario(const std::string idUsuario) {
-  std::ifstream FileUsers;
+  std::ifstream FileUsers; // vai retornar todos os dados do usuário, menos o id.
 
   try {
     FileUsers.open(RepositorioUsuarios::_filePath);
-    std::string line = " ";
+    std::string line = " "; // variável que vai ser preenchida com as informações de cada linha do arquivo
+    // seu valor muda a cada linha lida
 
     while (std::getline(FileUsers, line)) {
       std::stringstream str(line);
 
       std::vector<std::string> auxiliar_dados = splitString(line, ' ');
-      if (auxiliar_dados[0] == idUsuario) {
-        std::string final_userstr = "";
-        for (std::size_t i = 1; i < auxiliar_dados.size(); i++) {
+      if (auxiliar_dados[0] == idUsuario) {// se o id do usuário na linha que eu estou for igual ao id do usuário que recebemos
+        std::string final_userstr = ""; // a string com os dados do usuário
+        for (std::size_t i = 1; i < auxiliar_dados.size(); i++) { // vamos pegar a partir do index 1 (email) e ir até o tipo (última informação do usuário na linha)
           final_userstr += auxiliar_dados[i];
           if (i != auxiliar_dados.size() - 1) {
             final_userstr += " ";
@@ -275,7 +277,7 @@ std::string RepositorioUsuarios::dados_usuario(const std::string idUsuario) {
         }
 
         FileUsers.close();
-        return final_userstr;
+        return final_userstr; // retorna a string
       }
     }
     FileUsers.close();
